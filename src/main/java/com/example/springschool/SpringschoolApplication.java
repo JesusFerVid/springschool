0ef277e133d67ae38af1e10cbee5ae715d7fafb4
Jesús Fernández Vidal <jesus.c.fernandez@accenture.com>
2023-12-29 09:34:55 +0100

    chore: Initial commit

---
+++ src/main/java/com/example/springschool/SpringschoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.springschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springschool/SpringschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
